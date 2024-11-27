bfecbb05901f41d3b766f292c217f26e2bb7be69
LazyTitan001 <rsr182003@gmail.com>
2024-11-27 15:39:28 +0530

    frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/SignIn";
+import { Dashboard } from "./pages/DashBoard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
